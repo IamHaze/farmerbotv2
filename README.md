2240cd41382b3200581effb33030470fe69235e3
IamHaze <masterhaze93@yahoo.com>
2022-06-24 09:52:42 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,81 @@
+
+# dankmemer-farm
+
+Simple script to farm Dankmemer coins with multiple accounts at once.
+
+**Requires:** `Discord Tokens` | `requests` module
+
+## Disclaimer
+
+**SOME COMMANDS NOLONGER WORK. THE SCRIPT IS NOLONGER ABLE TO AUTO-TRANSFER MONEY FROM A ALT ACCOUNT TO ANOTHER ACCOUNT! ALL OTHER COMMANDS STILL WORK AND CAN BE FULLY CUSTOMIZED!**
+
+I don't take responsibility if you get banned from a server or if your Tokens get locked
+
+You can run this script with 15 accounts at once before getting rate limited.
+
+**All accounts need access to the channel ID you provide. Make sure all accounts are in the server and have send message permission**
+
+## Usage
+This script was build for [Python 3.9](https://www.python.org/downloads/release/python-390/)
+
+```
+py main.py
+```
+
+How to use.
+------------
+**1.** Create a folder.
+
+**2.** Place the python file in the folder.
+
+**3.** Place a text file (`tokens.txt`) with discord Tokens in the folder.
+**Call the file "tokens.txt"**. If you change the name, the script won't find your Tokens.
+
+**4.** Edit the script and change the following variables.
+
+`channel_id` -> Set this to the ID of the channel you want to farm in.
+
+`id` -> Set this to your user ID. The bots will send their money to this account.
+
+**All your Tokens need access to the channel, so make sure they are joined, and have send message permission.**
+
+**5.** Run the script with `python3 main.py`
+
+**6. Profit**
+
+## Support
+Open a github issue
+
+You can join my Disocrd [server](https://daddyissue.org) (**NOT FOR SUPPORT**)
+
+You can open an issue on Github as well
+
+## FAQ
+> **Do i need multiple accounts?**
+
+No, you can run this with as many accounts as you like. Note that Discord will rate limit you if you send too many API requests.
+
+> **The file closes after i open it**
+
+You have to run the file via the command prompt / terminal. If you don't know how to do that, ask for help in [my discord server](https://daddyissue.org)
+
+> **How do i download Python**
+
+You can download the latest version of [Python3 here](https://www.python.org/downloads/)
+After downloading, run the file, and make sure you click the checkbox `add to path` as it will help you out later.
+
+> **Can i get banned**
+
+Yes. You can always get banned. Blacklisted by dankmemer or banned by discord. It's very unlikely tho.
+
+> **All the commands are saying they failed**
+
+Make sure your tokens are valid and that all accounts have permission to send messages in the channel you supplied. If you still get errors, open a Github issue
+> **How do i get channel id's and user id's**
+
+You can enable developer settings in Discord like this: `settings -> advanced -> developer settings` then right-click a channel or user and click `copy id`
+
+> **How do i get my Discord token**
+
+I can't explain this here, but **google it**
+
---
+++ main.py
@@ -0,0 +1,143 @@
+import requests
+import time
+import random
+from multiprocessing import Process
+#Imports
+
+#########################################
+           ###REPLACE THOSE###
+    
+channel_id = '989421845528842343'
+id = '316204134879592448'
+
+           ###REPLACE THOSE###
+#########################################
+
+print("""
+https://github.com/ScobraScope/dankmemer-farm/
+
+This script is able to automatic farm dankmemer credits on multiple accounts
+at once. It sends post requests to discords API and can run in the background.
+Make sure to change the following variables in the python file before running it:
+
+channel_id
+id
+
+If the script is not working at all, feel free to contact me on discord, https://discord.gg/kjsdxkbtjw
+
+Made by 6A31 - https://github.com/scobrascope
+GNU GENERAL PUBLIC LICENSE V3
+
+Please follow the license at https://github.com/ScobraScope/dankmemer-farm/blob/main/LICENSE.md
+---------------------------------------------------------------
+
+
+
+
+""")
+
+#███╗░░░███╗░█████╗░██████╗░███████╗
+#████╗░████║██╔══██╗██╔══██╗██╔════╝
+#██╔████╔██║███████║██║░░██║█████╗░░
+#██║╚██╔╝██║██╔══██║██║░░██║██╔══╝░░
+#██║░╚═╝░██║██║░░██║██████╔╝███████╗
+#╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝
+
+
+#██████╗░██╗░░░██╗
+#██╔══██╗╚██╗░██╔╝
+#██████╦╝░╚████╔╝░
+#██╔══██╗░░╚██╔╝░░
+#██████╦╝░░░██║░░░
+#╚═════╝░░░░╚═╝░░░
+
+
+#░█████╗░░█████╗░██████╗░░░███╗░░
+#██╔═══╝░██╔══██╗╚════██╗░████║░░
+#██████╗░███████║░█████╔╝██╔██║░░
+#██╔══██╗██╔══██║░╚═══██╗╚═╝██║░░
+#╚█████╔╝██║░░██║██████╔╝███████╗
+#░╚════╝░╚═╝░░╚═╝╚═════╝░╚══════╝
+
+
+if channel_id == 'The id of your channel to farm in': #DON'T TOUCH THIS, THIS IS JUST A CHECK!
+    print("ERROR: Make sure to specify a channel id. If you don't know how to obtain channel id's use Google.")
+    time.sleep(5)
+    quit()
+if channel_id == 'Your user id for receiving money': #DON'T TOUCH THIS, THIS IS JUST A CHECK!
+    print("ERROR: Please specify what user to send the money to. specify a user id above. If you don't know how to obtain user id's use Google")
+    time.sleep(5)
+    quit()
+
+
+try:
+    token = open("tokens.txt", "r")
+    tokenlist = token.read().split("\n") #Creates a list with all the Tokens from the file
+except:
+    print("-" * 20)
+    print("ERROR: No file with tokens detected. Please place a file in the same directory as the script, called 'tokens.txt'")
+
+def sendmessage(token):
+    message = "pls beg"
+    messagedig = "pls dig"
+    messagefish = "pls fish"
+    messagehunt = "pls hunt"
+    message2 = f"random text to bypass the bot protection AAA {random.randint(1, 99999) * random.randint(1, 9999) + random.randint(1,99999)}" #Sends a string with random numbers to trick dankmemer's macro detection
+    pay = f"pls trade 10000 <@{id}>"
+    headers = {'Authorization': token}
+    i = 0
+
+    while True:
+        try:
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagehunt})
+            if res.status_code == 200:
+                print("Hunted successfully")
+            else:
+                print(f"Failed to hunt. Errorcode {res}")
+            time.sleep(5)
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message})
+            if res.status_code == 200:
+                print("Begged successfully")
+            else:
+                print(f"Failed to beg. Errorcode {res}")
+            time.sleep(5)
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagefish})
+            if res.status_code == 200:
+                print("Fished successfully")
+            else:
+                print(f"Failed to fish. Errorcode {res}")
+            time.sleep(5)         
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': messagedig})
+            if res.status_code == 200:
+                print("Fished successfully")
+            else:
+                print(f"Failed to fish. Errorcode {res}")
+            time.sleep(5)
+            time.sleep(random.randint(1,6))
+
+            res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': message2})
+            print("Bypassed message send successfully")
+            time.sleep(25 + random.randint(1,5))
+            i += 1
+            if i > 2:
+                res = requests.post(f'https://discordapp.com/api/v6/channels/{channel_id}/messages', headers=headers, json={'content': pay})
+                if res.status_code == 200:
+                    print("Payout successful")
+                else:
+                    print(f"Failed to pay. Errorcode {res}")
+                i = 0
+
+        except:
+            print(f"Failed on token: {token}")
+            print("If the script is not working at all, feel free to contact me on discord, https://discord.gg/kjsdxkbtjw")
+            pass
+
+
+#Start script
+if __name__=='__main__':
+    for token in tokenlist:
+        p1 = Process(target=sendmessage, args=(token,))
+        p1.start()
+
+    
+    
